317ebf229231c64966505be162c3c6c641a1b318
sharepia <sharepia@gmail.com>
2023-11-23 09:34:16 +0900

    Teacher Recent Source, 2311230934

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore, useAlertStore } from '@/stores';
+import { Home } from '@/views';
+import accountRoutes from './account.routes';
+import usersRoutes from './users.routes';
+import pokemonsRoutes from './pokemons.routes';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    
-  ]
-})
+export const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    linkActiveClass: 'active',
+    routes: [
+        { path: '/', component: Home },
+        { ...accountRoutes },
+        { ...usersRoutes },
+        { ...pokemonsRoutes },
+        // catch all redirect to home page
+        { path: '/:pathMatch(.*)*', redirect: '/' }
+    ]
+});
 
-export default router
+router.beforeEach(async (to) => {
+    // clear alert on route change
+    const alertStore = useAlertStore();
+    alertStore.clear();
+
+    // redirect to login page if not logged in and trying to access a restricted page 
+    const publicPages = ['/account/login', '/account/register'];
+    const authRequired = !publicPages.includes(to.path);
+    const authStore = useAuthStore();
+
+    console.log('to.path ' + to.path)
+    console.log('authRequired ' + authRequired)
+    console.log('!authStore.user ' + !authStore.user)
+    console.log(authRequired && !authStore.user)
+
+    if (authRequired && !authStore.user) {
+        authStore.returnUrl = to.fullPath;
+        return '/account/login';
+    }
+});
---
+++ src/stores/users.store.js
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia';
+import { fetchWrapper } from '@/helpers';
+import { useAuthStore } from '@/stores';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useUsersStore = defineStore({
+    id: 'users',
+    state: () => ({
+        user: {},
+        pageInfo: {},
+        users: {},
+    }),
+    actions: {
+        async register(user) {
+            await fetchWrapper.post(`${baseUrl}/auth/register`, user);
+        },
+        async getAll(params) {
+            this.users = { loading: true };
+            try {
+                const queryString = new URLSearchParams(params).toString();
+                const { content, pageSize, totalElements, totalPages } =
+                    await fetchWrapper.get(`${baseUrl}/admin/users?${queryString}`);
+                this.users = content;
+                this.pageInfo = { pageSize, totalElements, totalPages }
+            } catch (error) {
+                this.users = { error };
+            }
+        },
+        async getById(id) {
+            this.user = { loading: true };
+            try {
+                this.user = await fetchWrapper.get(`${baseUrl}/admin/users/${id}`);
+            } catch (error) {
+                this.user = { error };
+            }
+        },
+        async update(id, params) {
+            await fetchWrapper.put(`${baseUrl}/admin/users/${id}`, params);
+
+            // update stored user if the logged in user updated their own record
+            const authStore = useAuthStore();
+            if (id === authStore.user.id) {
+                // update local storage
+                const user = { ...authStore.user, ...params };
+                localStorage.setItem('user', JSON.stringify(user));
+
+                // update auth user in pinia state
+                authStore.user = user;
+            }
+        },
+    }
+});
